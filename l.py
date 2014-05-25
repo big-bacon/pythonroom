4bd54ab9c7e6ded80244c1a302dc7ab0369ec492
big-bacon <akemi_w@yahoo.com>
2014-05-25 15:27:23 -0700

    pythonroom update

--- l.py
+++ l.py
@@ -1,1 +1,17 @@
 # author: big-bacon
+import turtle
+
+r = turtle.Turtle()
+sides = 9
+angle = 360.0
+colors = ["red", "green", "yellow", "blue"]
+distances = range(20, 120)
+
+for distance in distances:
+	r.left(100)
+	for c in colors:
+		r.color(c)
+		r.forward(distance)
+		r.left(1000)
+		
+		r.speed(0)
