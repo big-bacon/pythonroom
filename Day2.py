a439218dbfced808841bb76a9ae2854c8114f43e
big-bacon <akemi_w@yahoo.com>
2014-06-01 14:30:45 -0700

    pythonroom update

--- Day2.py
+++ Day2.py
@@ -4,4 +4,7 @@ you = you.lower()
 
 if you[0] == "a" or you[0] == "e" or you[0] == "i" or you[0] == "o" or you[0] == "u" or you[0] == "y":
 	you = you + "yay"
+
+else:
+    you = you + you[1:]+ you[:1]+ "ay"
 print you
