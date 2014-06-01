57e0f8bde12e2868078ce6e72f527669875d7a84
big-bacon <akemi_w@yahoo.com>
2014-06-01 14:39:20 -0700

    pythonroom update

--- Day2.py
+++ Day2.py
@@ -1,10 +1,11 @@
 # author: big-bacon
 you = input("talk")
 you = you.lower()
+output = ""
 
 if you[0] == "a" or you[0] == "e" or you[0] == "i" or you[0] == "o" or you[0] == "u" or you[0] == "y":
-	you = you + "yay"
+	output = output + you + "yay"
 
 else:
-    you = you + you[1:]+ you[:1]+ "ay"
-print you
+    output = output + you[1:]+ you[:1]+ "ay"
+print output
