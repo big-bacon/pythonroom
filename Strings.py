92e58bd3962cbf404c7eee63b0ee0186aba7abb0
big-bacon <akemi_w@yahoo.com>
2014-06-01 13:29:25 -0700

    pythonroom update

--- Strings.py
+++ Strings.py
@@ -1,14 +1,6 @@
 # author: big-bacon
-import random
-NumPeople = 6
-Pic = 100
-for j in range (0,Pic):
-	ok = True 
-	for i in range(0,NumPeople):
-		r = random.randint(0,100)
-		
-		if r > 50: 
-			ok = False
-	if ok == True:
-		print "Nobody blinked after taking %d pictures." %j
-		break
+l = input("do you want to build a snowman?")
+l = l.lower()
+vowels [ "a", "e", "i", "o", "u" ]
+if word == "a" or word 
+v = ay 
