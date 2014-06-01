2b2a804e3a4fa5a7ef0dd247f0d5c687df9b0135
big-bacon <akemi_w@yahoo.com>
2014-06-01 15:27:13 -0700

    pythonroom update

---
+++ palindrome.py
@@ -0,0 +1,12 @@
+y = input("hi")
+lenghth = len(y)
+for i in range (0, length): 
+#word renders the same forward & backwards	
+
+#compare 1st & last #
+
+#do some code
+
+#if not equal,
+
+#do some code
