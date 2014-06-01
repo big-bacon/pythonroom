eb18cc85938fe3c432af10802d83a0ba61969652
big-bacon <akemi_w@yahoo.com>
2014-06-01 14:07:13 -0700

    pythonroom update

--- Day2.py
+++ Day2.py
@@ -1,3 +1,8 @@
 # author: big-bacon
 you = input("talk")
 you = you.lower()
+print you
+
+if word[0] == "a" or word[0] == "e" or word[0] == "i" or word[0] == "o" or word[0] == "u" or word[0] == "y":
+	you = you + "ay"
+	print you
